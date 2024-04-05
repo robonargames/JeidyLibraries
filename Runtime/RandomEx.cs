7e57e8906bdb6f4d90ddc868c7613068f1c2bd96
contacoonta <contacoonta@gmail.com>
2024-04-06 02:09:14 +0900

    Revert "Revert "Revert "new cummot"""
    This reverts commit 459fbf2d855ec098ef44296126c8c54e7ffd54e3.

--- Runtime/RandomEx.cs
+++ Runtime/RandomEx.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-static public class RandomEx
-{
 
-    //var exclude = new HashSet<int>() { 5, 7, 17, 23 };
-    static public int GetRandomNumber(HashSet<int> exclude, int min, int count )
+namespace RobonarGames
+{
+    static public class RandomEx
     {
-        var range = Enumerable.Range(min, count).Where(i => !exclude.Contains(i));
-        var rand = new System.Random();
-        int index = rand.Next(min, count - exclude.Count);
-        return range.ElementAt(index);
+
+        //var exclude = new HashSet<int>() { 5, 7, 17, 23 };
+        static public int GetRandomNumber(HashSet<int> exclude, int min, int count)
+        {
+            var range = Enumerable.Range(min, count).Where(i => !exclude.Contains(i));
+            var rand = new System.Random();
+            int index = rand.Next(min, count - exclude.Count);
+            return range.ElementAt(index);
+        }
     }
+
 }
